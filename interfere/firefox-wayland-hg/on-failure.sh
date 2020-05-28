33070f025354f2359d379febb2f99a58835d1008
PedroHLC <root@pedrohlc.com>
2020-05-28 14:10:30 -0300

    Remove interfere to add submodule; Update TkG; Block mingw-w64-binutils

--- interfere/firefox-wayland-hg/on-failure.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-[ -f ../firefox-wayland-hg.log ] && \
-telegram-send \
-	--config ~/.config/telegram-send-group.conf \
-	--sile "[BOT ALERT] @thotypous your package \"firefox-wayland-hg\" build failed, here's the log: " \
-	-f ../firefox-wayland-hg.log
